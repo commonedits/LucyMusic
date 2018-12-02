3979181d35b1873836e6de493573286d77a11d38
Matthew S. Lewis <matthew@commonedits.com>
2018-12-02 14:05:48 -0600

    Lucy Music Code
    Lucy Music Code

---
+++ Lucy Music Code/viewer.js
@@ -0,0 +1,50 @@
+var token = "";
+var tuid = "";
+
+var twitch = window.Twitch.ext;
+
+function setAuth(token) {
+  Object.keys(requests).forEach((req) => {
+      twitch.rig.log('Setting auth headers');
+      requests[req].headers = { 'Authorization': 'Bearer ' + token }
+  });
+}
+
+twitch.onContext(function(context) {
+  twitch.rig.log(context);
+});
+
+twitch.onAuthorized(function(auth) {
+  // save our credentials
+  token = auth.token;
+  tuid = auth.userId;
+
+  // enable the button
+  $('#cycle').removeAttr('disabled');
+
+  setAuth(token);
+});
+
+function updateBlock(hex) {
+    $('#color').css('background-color', hex);
+}
+
+function getRandomColor() {
+  return '#'+Math.floor(Math.random()*16777215).toString(16);
+}
+
+$(function() {
+    $('#cycle').prop('disabled', false);
+
+    $('#cycle').click(function() {
+      updateBlock(getRandomColor());
+    });
+});
+function myFunction() {
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
